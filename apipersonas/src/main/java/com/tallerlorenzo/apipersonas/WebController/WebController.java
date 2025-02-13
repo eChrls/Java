87ef840f43f36b0bd669102aba24034b70dd44b3
jtormos373 <jtormos373@g.educaand.es>
2025-02-13 10:21:37 +0100

    latest

--- apipersonas/src/main/java/com/tallerlorenzo/apipersonas/WebController/WebController.java
+++ apipersonas/src/main/java/com/tallerlorenzo/apipersonas/WebController/WebController.java
@@ -1,8 +1,21 @@
 package com.tallerlorenzo.apipersonas.WebController;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@RequestMapping("/api")
 @RestController
 public class WebController {
+
+    //GET  /helloWorld -> "Hello World"
+    @GetMapping("/helloWorld")
+    public String getMethodName() {
+        return "Hello World";
+
+    }
+    
+
     
 }
