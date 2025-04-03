69d3418566222a3c99002a5811e0365773817b90
eChrls <ecom.jct@gmail.com>
2025-04-03 16:54:59 +0200

    examenAbril

---
+++ EXAMENES/p62AcademiaJCTM/src/main/java/juego/p62academiajctm/SolicitarBaja.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package juego.p62academiajctm;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author carlos
+ */
+public interface SolicitarBaja {
+    
+    public LocalDate solicitarBaja();
+    
+
+}
