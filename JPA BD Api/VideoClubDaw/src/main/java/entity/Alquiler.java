5e5695c86ee88f6c8836fbd2fd867351610ce391
eChrls <ecom.jct@gmail.com>
2025-05-24 21:18:06 +0200

    proyectoFinal_Estructura

--- JPA BD Api/VideoClubDaw/src/main/java/entity/Alquiler.java
+++ JPA BD Api/VideoClubDaw/src/main/java/entity/Alquiler.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package entity;
 
-/**
- *
- * @author carlos
+import javax.persistence.*;
+import java.util.Date;
+
+/*
+ * Entidad Alquiler
+ * Representa el alquiler de una película por un usuario.
+ * Relación N:1 con Usuario y Pelicula.
  */
+@Entity
+@Table(name = "alquiler")
 public class Alquiler {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_alquiler")
+    private Long idAlquiler;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "fecha_alquiler")
+    private Date fechaAlquiler;
+
+    // Relación N:1 con Usuario
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    // Relación N:1 con Pelicula
+    @ManyToOne
+    @JoinColumn(name = "id_pelicula")
+    private Pelicula pelicula;
+
+    public Alquiler() {}
+
+    // Getters y setters
+    public Long getIdAlquiler() { return idAlquiler; }
+    public void setIdAlquiler(Long idAlquiler) { this.idAlquiler = idAlquiler; }
+
+    public Date getFechaAlquiler() { return fechaAlquiler; }
+    public void setFechaAlquiler(Date fechaAlquiler) { this.fechaAlquiler = fechaAlquiler; }
+
+    public Usuario getUsuario() { return usuario; }
+    public void setUsuario(Usuario usuario) { this.usuario = usuario; }
+
+    public Pelicula getPelicula() { return pelicula; }
+    public void setPelicula(Pelicula pelicula) { this.pelicula = pelicula; }
 }
--- JPA BD Api/VideoClubDaw/src/main/java/entity/Pelicula.java
+++ JPA BD Api/VideoClubDaw/src/main/java/entity/Pelicula.java
@@ -1,13 +1,89 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package entity;
 
-/**
- *
- * @author carlos
+import javax.persistence.*;
+import java.util.List;
+
+/*
+ * Entidad Pelicula
+ * Representa una película disponible en el videoclub.
+ * Cada película puede tener varios alquileres (relación 1:N).
+ * Cada película tiene una ficha técnica (relación 1:1).
  */
+@Entity
+@Table(name = "pelicula")
 public class Pelicula {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pelicula")
+    private Long idPelicula;
+
+    @Column(nullable = false, length = 200)
+    private String titulo;
+
+    @Column(name = "anyo_estreno")
+    private Integer anyoEstreno;
+
+    @Column(name = "portada")
+    private String portada;
+
+    // Relación 1:N con Alquiler
+    @OneToMany(mappedBy = "pelicula", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Alquiler> alquileres;
+
+    // Relación 1:1 con FichaTecnica
+    @OneToOne(mappedBy = "pelicula", cascade = CascadeType.ALL, orphanRemoval = true)
+    private FichaTecnica fichaTecnica;
+
+    public Pelicula() {
+    }
+
+    // Getters y setters
+    public Long getIdPelicula() {
+        return idPelicula;
+    }
+
+    public void setIdPelicula(Long idPelicula) {
+        this.idPelicula = idPelicula;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getAnyoEstreno() {
+        return anyoEstreno;
+    }
+
+    public void setAnyoEstreno(Integer anyoEstreno) {
+        this.anyoEstreno = anyoEstreno;
+    }
+
+    public List<Alquiler> getAlquileres() {
+        return alquileres;
+    }
+
+    public void setAlquileres(List<Alquiler> alquileres) {
+        this.alquileres = alquileres;
+    }
+
+    public FichaTecnica getFichaTecnica() {
+        return fichaTecnica;
+    }
+
+    public void setFichaTecnica(FichaTecnica fichaTecnica) {
+        this.fichaTecnica = fichaTecnica;
+    }
+
+    public String getPortada() {
+        return portada;
+    }
+
+    public void setPortada(String portada) {
+        this.portada = portada;
+    }
 }
